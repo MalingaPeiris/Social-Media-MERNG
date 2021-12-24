32c5cff77b2bba7e1f951534978006c1f92a4df2
MalingaPeiris <63008840+MalingaPeiris@users.noreply.github.com>
2021-12-24 18:23:00 +0530

    deleteComment

--- graphQL/resolvers/comments.js
+++ graphQL/resolvers/comments.js
@@ -1,4 +1,4 @@
-const { UserInputError } = require("apollo-server");
+const { AuthenticationError, UserInputError } = require("apollo-server");
 
 const checkAuth = require("../../util/checkAuth");
 const Post = require("../../Models/Post");
@@ -27,6 +27,23 @@ module.exports = {
         await post.save();
         return post;
       } else throw new UserInputError("Post Not Found!");
+    },
+    async deleteComment(_, { postId, commentId }, context) {
+      const { username } = checkAuth(context);
+
+      const post = await Post.findById(postId);
+
+      if (post) {
+        const commentIndex = post.comments.findIndex((c) => c.id === commentId);
+
+        if (post.comments[commentIndex].username === username) {
+          post.comments.splice(commentIndex, 1);
+          await post.save();
+          return post;
+        } else new AuthenticationError("Action not Allowed!");
+      } else {
+        throw new AuthenticationError("Post Not Found!");
+      }
     },
   },
 };
