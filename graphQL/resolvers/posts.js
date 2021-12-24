807ef112c39d4b3906196f92723c59ff77cf905c
MalingaPeiris <63008840+MalingaPeiris@users.noreply.github.com>
2021-12-24 18:39:31 +0530

    likes

--- graphQL/resolvers/posts.js
+++ graphQL/resolvers/posts.js
@@ -1,4 +1,4 @@
-const { AuthenticationError } = require("apollo-server");
+const { AuthenticationError, UserInputError } = require("apollo-server");
 
 const Post = require("../../Models/Post");
 const checkAuth = require("../../util/checkAuth");
@@ -57,6 +57,25 @@ module.exports = {
       } catch (err) {
         throw new Error(err);
       }
+    },
+    async likePost(_, { postId }, context) {
+      const { username } = checkAuth(context);
+
+      const post = await Post.findById(postId);
+      if (post) {
+        if (post.likes.find((like) => like.username === username)) {
+          //if the post is already liked, unlike it
+          post.likes = post.likes.filter((like) => like.username !== username);
+        } else {
+          // not likes, like post
+          post.likes.push({
+            username,
+            createdAt: new Date().toISOString(),
+          });
+        }
+        await post.save();
+        return post;
+      } else throw new UserInputError("Post Not Found!");
     },
   },
 };
