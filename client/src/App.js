f29bd6589896d495983d700253ec4c947b731eda
MalingaPeiris <63008840+MalingaPeiris@users.noreply.github.com>
2021-12-26 15:02:47 +0530

    react client

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,25 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+//import "semantic-ui-css/semantic.min.css";
 import "./App.css";
+
+//import MenuBar from "./component/MenuBar"
+import Home from "./Pages/Home";
+import Login from "./Pages/Login";
+import Register from "./Pages/Register";
 
 function App() {
   return (
-    <div>
-      <h1>Hellow World!!!</h1>
-    </div>
+    //<div><h1>asd</h1>console.log('hi')</div>
+
+    <Router>
+      <Routes>
+        <Route exact path="/" component={Home}></Route>
+        <Route exact path="/login" component={Login}></Route>
+        <Route exact path="/register" component={Register}></Route>
+      </Routes>
+    </Router>
   );
 }
 
